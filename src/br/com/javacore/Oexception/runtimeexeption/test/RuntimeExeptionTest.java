d991942fd881617babbb78b3872aa987cce44182
Felipe Lima <felipelimademedeiros@gmail.com>
2020-03-04 20:36:32 -0300

    Try-Catch

--- src/br/com/javacore/Oexception/runtimeexeption/test/RuntimeExeptionTest.java
+++ src/br/com/javacore/Oexception/runtimeexeption/test/RuntimeExeptionTest.java
@@ -3,17 +3,24 @@ package br.com.javacore.Oexception.runtimeexeption.test;
 public class RuntimeExeptionTest {
     public static void main(String[] args) {
 
-        //ArithmeticException
-        int a = 10/0;
-        System.out.println(a);
+        try {
+            int b[] = new int[2];
+            System.out.println(b[20]);
+            System.out.println("Imprimindo dpeois da excecao");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Fora do catch");
 
-        //ArrayIndexOutOfBoundsException
-        int b[] = new int[2];
-        System.out.println(b[20]);
+
+        //ArithmeticException
+        //int a = 10/0;
+        //System.out.println(a);
 
         //NullPointerException
-        Object o = null;
-        System.out.println(o.toString());
+        //Object o = null;
+        //System.out.println(o.toString());
+        //ArrayIndexOutOfBoundsException
 
     }
 }
