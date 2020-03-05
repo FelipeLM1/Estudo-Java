5108fc9ebe4fae7dde05b03c1851dd9bad9ec1dd
Felipe Lima <felipelimademedeiros@gmail.com>
2020-03-05 17:21:15 -0300

    lançamento de exceção

--- src/br/com/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
+++ src/br/com/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
@@ -1,23 +1,29 @@
 package br.com.javacore.Oexception.checkedexception.test;
 
 import java.io.File;
+import java.io.IOError;
 import java.io.IOException;
 
 public class CheckedExceptionTest {
     public static void main(String[] args) {
-        criarArquivo();
+        try {
+            criarArquivo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //IOException
-    public static void criarArquivo() {
+    public static void criarArquivo() throws IOException {
         File file = new File("Teste.txt");
         try {
             System.out.println("Arquivo criado? " + file.createNewFile());
             System.out.println("Arquivo criado");
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Aquivo nao foi criado! ");
+            throw e;
         }
 
     }
 }
+
--- src/br/com/javacore/Oexception/runtimeexeption/test/RuntimeExeptionTest.java
+++ src/br/com/javacore/Oexception/runtimeexeption/test/RuntimeExeptionTest.java
@@ -2,25 +2,44 @@ package br.com.javacore.Oexception.runtimeexeption.test;
 
 public class RuntimeExeptionTest {
     public static void main(String[] args) {
-
         try {
-            int b[] = new int[2];
-            System.out.println(b[20]);
-            System.out.println("Imprimindo dpeois da excecao");
-        } catch (ArrayIndexOutOfBoundsException e) {
+            divisao(10, 0);
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        System.out.println("Fora do catch");
+        /*
+
+        try {
+                    int b[] = new int[2];
+                    System.out.println(b[20]);
+                    System.out.println("Imprimindo dpeois da excecao");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Fora do catch");
 
 
-        //ArithmeticException
-        //int a = 10/0;
-        //System.out.println(a);
+                //ArithmeticException
+                //int a = 10/0;
+                //System.out.println(a);
 
-        //NullPointerException
-        //Object o = null;
-        //System.out.println(o.toString());
-        //ArrayIndexOutOfBoundsException
+                //NullPointerException
+                //Object o = null;
+                //System.out.println(o.toString());
+                //ArrayIndexOutOfBoundsException
 
+
+        */
     }
+
+    private static void divisao(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Passe um valor diferente de 0 para o num2! ");
+        }
+        int result = num1/num2;
+        System.out.println(result);
+    }
+
+
 }
