bd9cce3d86d61bc584843f2006d517c82facb72f
Felipe Lima <felipelimademedeiros@gmail.com>
2020-04-07 21:03:18 -0300

    Removendo comentarios

--- Spring/codeblog/src/main/java/com/spring/codeblog/configuration/SecurityConfig.java
+++ Spring/codeblog/src/main/java/com/spring/codeblog/configuration/SecurityConfig.java
@@ -36,6 +36,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception{
         web.ignoring().antMatchers("/bootstrap/**");
-//        web.ignoring().antMatchers("/bootstrap/**", "/style/**");
+        
     }
 }
